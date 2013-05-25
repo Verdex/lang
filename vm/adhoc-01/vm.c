40340ded5e6589b2d0e8b4a0cbb1d31e3f26a314
David L. Inman <davidlevere@gmail.com>
2013-05-25 18:09:25 -0400

    initial work for add opcode (adding needed parameter, etc work)

--- vm/adhoc-01/vm.c
+++ vm/adhoc-01/vm.c
@@ -11,11 +11,7 @@
 
 
 typedef enum {
-    op_add_u8,
-    op_add_u16,
     op_add_u32,
-    op_add_s8,
-    op_add_s16,
     op_add_s32,
     // TODO add other basic ops
     // TODO mov
@@ -26,9 +22,29 @@ typedef enum {
 
     op_exit
 } opcode_t;
-     
+    
+typedef enum {
+    loc_stack,
+    loc_glob,
+    loc_ip,
+    loc_sp,
+    loc_bp,
+    loc_ret,
+    loc_accum,
+    loc_gen,
+    loc_null,
+} loc_t;
+
+typedef struct {
+    loc_t type;
+    bool deref;
+    uint64_t offset;
+} param_t;
+
 typedef struct {
     opcode_t opcode;
+    param_t dest;
+    param_t src;
 
     // dest needs to be a register or a register dereferenced with offset of possibly zero
     // source can then be a register, a register dereferenced (with offset of possily zero),
@@ -61,7 +77,7 @@ typedef struct {
     // TODO decide if these are actually going to be useful
     uint64_t ret;
     uint64_t accum;
-    uint8_t* regs;
+    uint64_t gen;
 
     // TODO:  throw a GC interface in here eventually
 } vm_t;
@@ -71,19 +87,45 @@ typedef struct {
 // any pointers that point to it.  Try not to do that without
 // thinking it through first.
 
+static void* get( vm_t* vm, param_t* p ) {
+    switch ( p->type ) {
+        case loc_stack:
+        case loc_glob:
+            return vm->global; // TODO this isnt right ... stack will need similar treatment
+        case loc_ip:
+            return vm->ip;
+        case loc_sp:
+            return vm->sp;
+        case loc_bp:
+            return vm->bp;
+        case loc_ret:
+            return &vm->ret;
+        case loc_accum:
+            return &vm->accum;
+        case loc_gen:
+            return &vm->gen;
+        case loc_null:
+            return NULL;
+    }
+    return NULL;
+}
+
 void vm_run( vm_t* vm ) {
     // TODO make sure vm isnt null (or is that really going to be an issue? 
     // ... like, there's a lot of things that someone can do to make
     // input invalid)
 
     while ( vm->ip->opcode != op_exit ) {
-    
-        switch ( vm->ip->opcode ) {
+        instr_t c = *vm->ip;
+        switch ( c.opcode ) {
 
-            case op_add_u8:
-                printf( "here\n" );
-                vm->ip++;
-                 
+            case op_add_u32:
+                { 
+                    uint32_t* v1 = get( vm, &c.dest );
+                    uint32_t* v2 = get( vm,  &c.src );
+                    printf( "%d\n", *v1 + *v2 );
+                    vm->ip++;
+                }                 
                 break;
             case op_exit:
             default: 
@@ -100,7 +142,7 @@ impossible:
 bool vm_init( vm_t* vm ) {
     // TODO make sure vm isnt null
 
-    vm = (vm_t*)malloc( sizeof( vm_t ) );
+    vm = malloc( sizeof( vm_t ) );
     if ( vm == NULL )
         goto error;
 
@@ -118,9 +160,14 @@ error:
 int main() {
 
     vm_t vm;
-    instr_t code[] = { { op_add_u8 }, { op_add_u8 }, { op_exit } };
+    instr_t code[] = { 
+        { op_add_u32, { loc_gen, false, 0 }, { loc_gen, false, 0 } }, 
+        { op_add_u32, { loc_gen, false, 0 }, { loc_gen, false, 0 } }, 
+        { op_exit, { loc_null, false, 0 }, { loc_null, false, 0 } } 
+    };
     vm.code = code;
     vm.ip = vm.code;
+    vm.gen = 1;
     vm_run( &vm );
     return 0;
 }
