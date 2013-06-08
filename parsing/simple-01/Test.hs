98b5d0502d9b8916c3bb61c088a1f697e261497e
David L. Inman <davidlevere@gmail.com>
2013-06-07 22:28:46 -0400

    added another simmple type test

--- parsing/simple-01/Test.hs
+++ parsing/simple-01/Test.hs
@@ -8,5 +8,6 @@ testSimple input = runParser simpleType () "" input
 
 simple01 = testSimple "Type"
 simple02 = testSimple "type"
+simple03 = testSimple "BlA76_`"
 
-main = print [simple01, simple02] 
+main = print [simple01, simple02, simple03] 
