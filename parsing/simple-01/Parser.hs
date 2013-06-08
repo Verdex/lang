841b6f4a6592b2a171b6ff5cf35ad1a84ec17224
David L. Inman <davidlevere@gmail.com>
2013-06-08 13:18:11 -0400

    outline for function types

--- parsing/simple-01/Parser.hs
+++ parsing/simple-01/Parser.hs
@@ -14,3 +14,13 @@ simpleType =
         r <- many (alphaNum <|> oneOf ['`', '_']) 
         return (fl : r)
 
+ikky :: Parsec String () String
+ikky = 
+    do 
+        t1 <- simpleType
+        -- optional spaces
+        -- optional arrow
+        -- optional spaces
+        -- t2 <- simple type
+        -- recurse?
+
