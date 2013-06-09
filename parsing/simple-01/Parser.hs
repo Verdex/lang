a0071f599e98e96114e5abe1c711a11be44e72d2
David L. Inman <davidlevere@gmail.com>
2013-06-09 00:19:42 -0400

    have a more or less working definition for type parsing ... inf loops right now ... Im guessing left recursion isn't supported (although that doesn't quite look right)
    will finish investigating later

--- parsing/simple-01/Parser.hs
+++ parsing/simple-01/Parser.hs
@@ -7,22 +7,44 @@ import Text.Parsec.Combinator
 import Text.Parsec.Prim
 
 
-simpleType :: Parsec String () String
+simpleType :: Parsec String () Func 
 simpleType =  
     do 
         fl <- upper
         r <- many (alphaNum <|> oneOf ['`', '_']) 
-        return (fl : r)
+        return (Simp $ fl : r)
 
-spaceArrow :: Parsec String () ()
+spaceArrow :: Parsec String () () -- might be able to use adjoint
 spaceArrow =
     do
         optional spaces
         string "->"
         optional spaces
         return ()
+-- TODO move to another file (consider refactor)
+type TypeName = String
+data Func = Paren Func
+    | Arrow Func Func
+    | Simp TypeName
+    deriving Show
 
-jabber :: Parsec String () [String]
-jabber = sepBy1 simpleType spaceArrow
+case1 :: Parsec String () Func 
+case1 = 
+    do 
+        char '('
+        t <- typeP
+        char ')'
+        return (Paren t)
 
+case2 :: Parsec String () Func
+case2 = 
+    do 
+        t1 <- typeP
+        spaceArrow
+        t2 <- typeP
+        return (Arrow t1 t2)
+
+-- TODO loops forever? ... left recursion?
+typeP :: Parsec String () Func
+typeP = case1 <|> case2 <|> simpleType
 
--- parsing/simple-01/Test.hs
+++ parsing/simple-01/Test.hs
@@ -5,13 +5,13 @@ import Text.Parsec.Prim
 import Parser
 
 testSimple input = runParser simpleType () "" input
-testFuncType input = runParser jabber () "" input
+testFuncType input = runParser typeP () "" input
 
 simple01 = testSimple "Type"
 simple02 = testSimple "type"
 simple03 = testSimple "BlA76_`"
 
 func01 = testFuncType "Type"
-func02 = testFuncType "Type -> TyPe_01` -> Int"
+func02 = testFuncType "(Type -> TyPe_01`) -> Int"
 
 main = print [func01,func02]
