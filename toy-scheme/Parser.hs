9dc67b6ecf64598c14763a56b45e623e00e9722f
David L. Inman <davidlevere@gmail.com>
2013-07-26 12:37:18 -0400

    symbol funciton added
    spaces function added
    
    crapy readExpr function added
    (I dont particularlly care for the slow build up,
    but I guess this is faster than trying to do it
    by myself)

--- toy-scheme/Parser.hs
+++ toy-scheme/Parser.hs
@@ -3,3 +3,23 @@ module Parser where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+symbol :: Parser Char
+-- very clever, haskell strings are just char arrays 
+-- (feels like an impedance mismatch waiting to happen)
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+-- TODO:  clearly this cant be the final status of this function
+-- normally i dont like case b/c of weird parsing issues that ive had in the past
+-- although i dont like its syntactic appearence it looks (semantically) a lot like agda "with" 
+-- which feels like a win (at least with does ... not sure about case...) 
+-- hey, now >> makes an appearence ... but shouldnt we be using adjoints? /shrug
+readExpr input = case parse (spaces >> symbol) "" input of
+    Left err -> "no match: " ++ show err
+    Right val -> "Symbol!!"
+
+spaces :: Parser ()
+-- not sure why we aren't using the official 'spaces' function 
+-- ... maybe they're trying to make this into a learning experience
+spaces = skipMany1 space
