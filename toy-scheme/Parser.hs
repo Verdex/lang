7381f13a1450be65a72cd47876a9d93b109d1b23
David L. Inman <davidlevere@gmail.com>
2013-07-26 13:28:03 -0400

    adding parse string functionality

--- toy-scheme/Parser.hs
+++ toy-scheme/Parser.hs
@@ -4,6 +4,8 @@ module Parser where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+import LangStuff
+
 symbol :: Parser Char
 -- very clever, haskell strings are just char arrays 
 -- (feels like an impedance mismatch waiting to happen)
@@ -14,7 +16,6 @@ readExpr :: String -> String
 -- normally i dont like case b/c of weird parsing issues that ive had in the past
 -- although i dont like its syntactic appearence it looks (semantically) a lot like agda "with" 
 -- which feels like a win (at least with does ... not sure about case...) 
--- hey, now >> makes an appearence ... but shouldnt we be using adjoints? /shrug
 readExpr input = case parse (spaces >> symbol) "" input of
     Left err -> "no match: " ++ show err
     Right val -> "Symbol!!"
@@ -23,3 +24,14 @@ spaces :: Parser ()
 -- not sure why we aren't using the official 'spaces' function 
 -- ... maybe they're trying to make this into a learning experience
 spaces = skipMany1 space
+
+parseString :: Parser LispVal
+-- bleh, escaping " will probably suck (although honestly, raw strings are pretty awesome
+-- so maybe I'll just skip the whole escapsing thing altogether and go for that instead)
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+
