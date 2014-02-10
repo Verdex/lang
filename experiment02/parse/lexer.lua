a0582f2d0dbe9d8ba053c300eabcff026707c6f1
David L. Inman <davidlevere@gmail.com>
2014-02-09 23:03:02 -0500

    trying to explore how I want basic syntax to look and whether or not that works out for a simple lexer

---
+++ experiment02/parse/lexer.lua
@@ -0,0 +1,49 @@
+
+module( ..., package.seeall )
+
+
+--[[
+
+looks like pythons syntax works b/c of colons at
+the end of a line
+
+def blah:
+
+def blah a b c :
+
+def blah
+    a 
+    b
+    c:
+
+def blah 
+    a b
+    c:
+
+def blah a b c:
+
+might be able to pull off haskell syntax by staying
+in pure mode ... if basically you only have one function
+application (with a bunch of sub function applications) 
+maybe that works out okay for the parser
+
+let blah = 
+
+blah = 
+
+blah a b c = 
+
+
+goal is no unnecessary parens, no commas, no end character
+
+--]]
+
+--[[
+    input string
+    ouput array lexeme
+--]]
+function lex( str )
+
+    
+
+end
