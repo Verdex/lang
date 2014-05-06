b3de50284f1ab4e6763e7b8f8d22840dc6791aba
David L. Inman <davidlevere@gmail.com>
2014-05-05 23:20:55 -0400

    getting lang_parse setup

---
+++ LC/untyped_assign/lang_parse.lua
@@ -0,0 +1,18 @@
+
+require "data"
+require "parse"
+
+module( ..., package.seeall )
+
+local mk_string = parse.mk_string
+local bind = parse.bind
+local unit = parse.unit
+local endStream = parse.endStream
+local alternative = parse.alternative
+local zeroOrMore = parse.zeroOrMore
+local oneOrMore = parse.oneOrMore
+local get_string = parse.get_string
+local get_anyDigit = parse.get_anyDigit
+local get_anyLetter = parse.get_anyLetter
+local get_whiteSpace = parse.get_whiteSpace
+
