7014e2b8848645221c9202e98430ae3e0d44efa6
David L. Inman <davidlevere@gmail.com>
2014-06-02 22:06:59 -0400

    compile totally worked, thanks be to God!
    top level assignments must be defined before they can be used
    (although in some cases it might still work ... top level assignment
    being assigned the result of a top level assignment not defined will break
    and top level being assigned application using top level assignment
    not yet defined will break)
    Not sure I want to fix that.  Seems like I probably have to change
    my compilation strategy to something more practical (top level
    are address or something).  Will handle in later revision, I'm sure.

--- LC/untyped_assign/compile.lua
+++ LC/untyped_assign/compile.lua
@@ -3,24 +3,6 @@ require "data"
 
 module( ..., package.seeall )
 
--- [assignment] -> [lua func]
-function compile( assignments )
-    local proto = {}
-    for _, assignment in ipairs( assignments ) do
-        local res, err = load( "return function( topLevel ) return " 
-                                .. compileHelp( assignment.expr, {} ) .. " end" )
-        if not res then
-            return nil, err
-        end
-        proto[#proto + 1] = { n = assignment.name, r = res() }
-    end
-    local funcs = {}
-    for _, p in ipairs( proto ) do
-        funcs[p.n] = p.r( funcs )
-    end
-    return funcs
-end
-
 local function compileHelp( expr, env )
     if data.is_paren( expr ) then
         return "( " .. compileHelp( expr.expr, env ) .. " )"
@@ -48,4 +30,21 @@ local function compileHelp( expr, env )
     end
 end
 
+-- [assignment] -> [lua func]
+function compile( assignments )
+    local proto = {}
+    for _, assignment in ipairs( assignments ) do
+        local res, err = load( "return function( topLevel ) return " 
+                                .. compileHelp( assignment.expr, {} ) .. " end" )
+        if not res then
+            return nil, err
+        end
+        proto[#proto + 1] = { n = assignment.name.name, r = res() }
+    end
+    local funcs = {}
+    for _, p in ipairs( proto ) do
+        funcs[p.n] = p.r( funcs )
+    end
+    return funcs
+end
 
--- LC/untyped_assign/example.lua
+++ LC/untyped_assign/example.lua
@@ -4,10 +4,11 @@ require "data"
 require "lang_parse"
 require "parse"
 require "check"
+require "compile"
 
---[=[local compile_lambda = untyped_assign.compile_lambda
+--[=[local  = untyped_assign.
 
-id_prog = compile_lambda [[
+id_prog =  [[
 
 id = \ i . i
 
@@ -135,26 +136,41 @@ _,pexpr11,_ = lang_parse.get_lambdaCalculus( str11 )
 assert( pexpr11 )
 assert( check.check( pexpr11 ) )
 
---[=[
-zero = compile_lambda[[\ f . \ v . v]]
-suc = compile_lambda[[\ n . \ f . \ v . f ( n f v )]]
-pred = compile_lambda[[ \n . \ f . \ v . n ( \ a . \ b . b ( a f ) ) ( \ k . v ) ( \ i . i )]]
-add = compile_lambda([[\ suc . \ n1 . \ n2 . n1 suc n2]])( suc )
-sub = compile_lambda([[\ pred . \ n1 . \ n2 . n1 pred n2]])( pred )
-mult = compile_lambda([[\ zero . \ add . \ n1 . \ n2 . n1 (add n2) zero]])( zero )( add )
-true_val = compile_lambda[[\ x . \ y . x]]
-false_val = compile_lambda[[\ x . \ y . y]]
-is_zero = compile_lambda(
-    [[\ true_val . \ false_val . \ n . n ( \ x . false_val ) true_val]])( true_val )( false_val )
+str12 = parse.mk_string [[
+    b = \ a . a ;
+    j = b b;
+]]
 
-fac = compile_lambda(
-    [[\ is_zero . \ suc . \ pred . \ mult . \ fac . \ num .
+_,pexpr12,_ = lang_parse.get_lambdaCalculus( str12 )
+assert( check.check( pexpr12 ) )
+
+str13 = parse.mk_string [[
+zero = \ f . \ v . v ; 
+suc = \ n . \ f . \ v . f ( n f v ) ;
+pred = \n . \ f . \ v . n ( \ a . \ b . b ( a f ) ) ( \ k . v ) ( \ i . i ) ;
+add = \ n1 . \ n2 . n1 suc n2 ;
+sub = \ n1 . \ n2 . n1 pred n2 ;
+mult = \ n1 . \ n2 . n1 (add n2) zero ;
+true_val = \ x . \ y . x ;
+false_val = \ x . \ y . y ;
+is_zero = \ n . n ( \ x . false_val ) true_val ;
+
+fac =  \ fac . \ num .
         is_zero 
             num 
             ( \ v . suc num v ) 
-            ( \ v . mult num ( fac ( pred num ) ) v )]])( is_zero )( suc )( pred )( mult )
+            ( \ v . mult num ( fac ( pred num ) ) v ) ;
 
-Y = compile_lambda[[\ f . ( \ x . f ( \ y . x x y ) ) ( \ x . f ( \ y . x x y ) )]]
+Y = \ f . ( \ x . f ( \ y . x x y ) ) ( \ x . f ( \ y . x x y ) ) ;
+
+factorial = Y fac ;
+
+]]
+
+_, pexpr13, _ = lang_parse.get_lambdaCalculus( str13 )
+assert( pexpr13 )
+
+funcs = compile.compile( pexpr13 )
 
 function eval_bool( b ) 
     return b( true )( false )
@@ -166,7 +182,8 @@ end
 
 
 -- The factorial of 5 is 120
-assert( eval_num( Y( fac )( suc( suc( suc( suc( suc( zero ) ) ) ) ) ) ) == 120 )
+suc = funcs.suc
+zero = funcs.zero
+assert( eval_num( funcs.factorial( suc( suc( suc( suc( suc( zero ) ) ) ) ) ) ) == 120 )
 
---]=]
 
