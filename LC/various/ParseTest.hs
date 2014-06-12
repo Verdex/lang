d2f774a8f36e3e6d351c95054c70bd33b97d7304
David L. Inman <davidlevere@gmail.com>
2014-06-11 21:45:23 -0400

    added additional tests for parse

--- LC/various/ParseTest.hs
+++ LC/various/ParseTest.hs
@@ -1,8 +1,8 @@
 
 module Main where
 
-import Data.List
 import Parse
+import Control.Applicative
 
 
 test parser input result = let o = parse parser input in
@@ -15,6 +15,13 @@ i = makeParseString
 tests = 
     [ test (getString "blah") (i "blah") (Just "blah", (4, "blah"))
     , test (getString "a") (i "aa") (Just "a", (1, "aa"))
+    , test (getString "a" <|> getString "b") (i "b") (Just "b", (1, "b"))
+    , test (getString "a" <|> getString "b") (i "a") (Just "a", (1, "a"))
+    , test (getString "a" <|> getString "b") (i "c") (Nothing, (0, "c"))
+    , test (some $ getString "a") (i "a") (Just ["a"], (1, "a"))
+    , test (some $ getString "a") (i "") (Nothing, (0, ""))
+    , test (many $ getString "a") (i "") (Just [], (0, ""))
+    , test (many $ getString "a") (i "aa") (Just ["a","a"], (2, "aa"))
     ]
 
 main = mapM_ putStrLn tests
