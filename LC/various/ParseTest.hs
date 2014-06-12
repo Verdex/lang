287cb7a7e3e5633d8d750b3e0182cc06f07a0349
David L. Inman <davidlevere@gmail.com>
2014-06-11 21:39:24 -0400

    test file improvements

--- LC/various/ParseTest.hs
+++ LC/various/ParseTest.hs
@@ -1,6 +1,20 @@
 
 module Main where
 
+import Data.List
 import Parse
 
-main = print $ parse (getString "blah") (makeParseString "blah")
+
+test parser input result = let o = parse parser input in
+    case result == o of
+        True -> "Pass " ++ show o
+        False -> "Fail " ++ show o
+
+i = makeParseString
+
+tests = 
+    [ test (getString "blah") (i "blah") (Just "blah", (4, "blah"))
+    , test (getString "a") (i "aa") (Just "a", (1, "aa"))
+    ]
+
+main = mapM_ putStrLn tests
