c9a3b64d180be0a8b47ea753dbaed1b0c043917b
David L. Inman <davidlevere@gmail.com>
2014-06-14 21:11:21 -0400

    stringify to lua function

--- LC/various/LuaCompile.hs
+++ LC/various/LuaCompile.hs
@@ -3,3 +3,9 @@ module LuaCompile where
 
 import ParseAst
 
+
+compileToLua = undefined
+
+stringifyToLua (Var name) = name 
+stringifyToLua (Abs param expr) = "( function ( " ++ param ++ " ) return " ++ stringifyToLua expr ++ " end )" 
+stringifyToLua (App e1 e2) = "( " ++ stringifyToLua e1 ++ " )( " ++ stringifyToLua e2 ++ " )" 
