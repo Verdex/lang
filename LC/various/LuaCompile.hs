2c28dd65baffa3812ffd45fd430558ccffed1bc0
David L. Inman <davidlevere@gmail.com>
2014-06-15 22:08:48 -0400

    Assignment setup, load setup

--- LC/various/LuaCompile.hs
+++ LC/various/LuaCompile.hs
@@ -13,3 +13,9 @@ stringifyToLua env (Abs param expr) =
     "( function ( " ++ param ++ " ) return " ++ stringifyToLua (param : env) expr ++ " end )" 
 stringifyToLua env (App e1 e2) = 
     "( " ++ stringifyToLua env e1 ++ " )( " ++ stringifyToLua env e2 ++ " )" 
+
+setupAssignments as = map convert as
+    where convert (Assignment name expr) = (name, 
+            "return function( topLevel ) return " ++ stringifyToLua [] expr ++ " end")
+
+loadSetup (name, func) = "funcs[\"" ++ name ++ "\"] = load( \"" ++ func ++ "\" )()( funcs )\n"
