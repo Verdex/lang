6d864446947b707dd0b823ede66a8030efdfd2b5
David L. Inman <davidlevere@gmail.com>
2014-06-15 22:54:02 -0400

    makefile; example/input

---
+++ LC/various/example.lua
@@ -0,0 +1,19 @@
+
+
+
+function eval_bool( b ) 
+    return b( true )( false )
+end
+
+function eval_num( n )
+    return n( function ( v ) return v + 1 end )( 0 ) 
+end
+
+
+-- The factorial of 5 is 120
+suc = funcs.suc
+zero = funcs.zero
+assert( eval_num( funcs.factorial( suc( suc( suc( suc( suc( zero ) ) ) ) ) ) ) == 120 )
+
+print(  eval_num( funcs.factorial( suc( suc( suc( suc( suc( zero ) ) ) ) ) ) ) )
+
