5c6a12d925e425b84ea213dd0815d1490765a3d2
David L. Inman <davidlevere@gmail.com>
2014-06-16 22:32:52 -0400

    added python output

--- LC/various/Compile.hs
+++ LC/various/Compile.hs
@@ -5,16 +5,18 @@ import Parse
 import ParseLang
 import ParseAst
 import LuaCompile
+import PythonCompile
 
 import System.Environment
 
-compile i = let (res, _) = parse getAssignments $ makeParseString i in
+compile compiler i = let (res, _) = parse getAssignments $ makeParseString i in
     case res of
-        Just r -> compileToLua r
+        Just r -> compiler r
         Nothing -> "Failure"
 
 main = do
     inputFileName <- fmap (head) getArgs 
     input <- readFile inputFileName
-    output <- writeFile "output.lua" $ compile input
+    output <- writeFile "output.lua" $ compile compileToLua input
+    output <- writeFile "output.py" $ compile compileToPython input
     return ()
