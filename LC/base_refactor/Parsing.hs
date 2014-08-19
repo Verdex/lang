c6fdafeef6fe824d1130e44d916d2180ae873dcf
David L. Inman <davidlevere@gmail.com>
2014-08-18 21:03:14 -0400

    functor definition for parser

--- LC/base_refactor/Parsing.hs
+++ LC/base_refactor/Parsing.hs
@@ -1,1 +1,26 @@
 
+module Parsing where
+
+import Control.Applicative
+import Data.Char
+
+
+type ParseString = (Int, String)
+
+makeParseString :: String -> ParseString
+makeParseString str = (0,str)
+
+data ParseResult a = 
+    Success a ParseString
+    | Failure 
+
+newtype Parser a = Parser( ParseString -> ParseResult a )
+
+parseWith (Parser p) = p
+
+
+instance Functor Parser where
+    fmap f parser = Parser $
+        \ ps -> case parseWith parser ps of
+            Success a ps' -> Success (f a) ps'
+            Failure -> Failure
