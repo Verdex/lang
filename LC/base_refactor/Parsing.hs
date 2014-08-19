6bf749391155270f0f6e3a3bf1e69b81bfd84f63
David L. Inman <davidlevere@gmail.com>
2014-08-18 22:21:44 -0400

    trying an alternate formating

--- LC/base_refactor/Parsing.hs
+++ LC/base_refactor/Parsing.hs
@@ -20,41 +20,69 @@ parseWith (Parser p) = p
 
 
 instance Functor Parser where
+
     fmap f parser = Parser $
-        \ ps -> case parseWith parser ps of
-            Success a ps' -> Success (f a) ps'
-            Failure -> Failure
+        \ ps -> 
+            case parseWith parser ps 
+            of
+                Success a ps' -> Success (f a) ps'
+                Failure -> Failure
+
 
 instance Applicative Parser where
+
     pure a = Parser $ \ ps -> Success a ps
+
     parser1 <*> parser2 = Parser $
-        \ ps -> case parseWith parser1 ps of
-            Failure -> Failure
-            Success f ps' -> case parseWith parser2 ps' of
+        \ ps -> 
+            case parseWith parser1 ps 
+            of
                 Failure -> Failure
-                Success a ps'' -> Success (f a) ps''
+                Success f ps' -> 
+                    case parseWith parser2 ps' 
+                    of
+                        Failure -> Failure
+                        Success a ps'' -> Success (f a) ps''
+
 
 instance Monad Parser where
+
     return = pure
+
     parser >>= gen = Parser $
-        \ ps -> case parseWith parser ps of
-            Failure -> Failure
-            Success a ps' -> parseWith (gen a) ps'
+        \ ps -> 
+            case parseWith parser ps 
+            of
+                Failure -> Failure
+                Success a ps' -> parseWith (gen a) ps'
+
 
 instance Alternative Parser where
+
     empty = Parser $ \ ps -> Failure
+
     parser1 <|> parser2 = Parser $ 
-        \ ps -> case parseWith parser1 ps of
-            Success a ps' -> Success a ps'
-            Failure -> case parseWith parser2 ps of
+        \ ps -> 
+            case parseWith parser1 ps 
+            of
                 Success a ps' -> Success a ps'
-                Failure -> Failure
+                Failure -> 
+                    case parseWith parser2 ps
+                    of
+                        Success a ps' -> Success a ps'
+                        Failure -> Failure
+
     many parser = Parser $ 
-        \ ps -> case parseWith parser ps of
-            Failure -> Success [] ps
-            Success a ps' -> case parseWith (many parser) ps' of
-                Failure -> Success [a] ps'
-                Success as ps'' -> Success (a : as) ps''
+        \ ps -> 
+            case parseWith parser ps 
+            of
+                Failure -> Success [] ps
+                Success a ps' -> 
+                    case parseWith (many parser) ps' 
+                    of
+                        Failure -> Success [a] ps'
+                        Success as ps'' -> Success (a : as) ps''
+
     some parser = 
         do
             a <- parser
@@ -62,21 +90,29 @@ instance Alternative Parser where
             return (a : as)
 
 getAnyX matcher transform = Parser $
-    \ (i,s) -> case i >= length s of
-        True -> Failure
-        False -> let x = s !! i in
-            case matcher x of
-                True -> Success (transform x) (i+1, s)
-                False -> Failure
+    \ (i,s) -> 
+        case i >= length s
+        of
+            True -> Failure
+            False -> 
+                let x = s !! i 
+                in
+                    case matcher x 
+                    of
+                        True -> Success (transform x) (i+1, s)
+                        False -> Failure
 
 getAnyDigit = getAnyX isDigit digitToInt
+
 getWhiteSpace = getAnyX isSpace id
+
 getAnyAlpha = getAnyX isLetter id
 
 getString str = Parser $
     \ (i,s) -> 
         let len = length str 
         in
-            case str == take len (drop i s) of
+            case str == take len (drop i s)
+            of
                 True -> Success str (i+len,s)
                 False -> Failure
