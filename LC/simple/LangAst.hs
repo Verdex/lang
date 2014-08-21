296feb487f539696a5e25b37bd6a5f886d589752
David L. Inman <davidlevere@gmail.com>
2014-08-20 23:21:57 -0400

    language ast adt

---
+++ LC/simple/LangAst.hs
@@ -0,0 +1,16 @@
+
+module LangAst where
+
+
+data Lambda =
+    Var String
+    | Abs String Lambda
+    | App Lambda Lambda
+
+    deriving (Show, Eq)
+
+
+data Assignment = Assignment String Lambda 
+
+    deriving (Show, Eq)
+
