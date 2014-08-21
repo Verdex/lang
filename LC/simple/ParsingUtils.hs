949b4f19735b889c304b07087251590a89fde258
David L. Inman <davidlevere@gmail.com>
2014-08-20 23:14:35 -0400

    parens parser

--- LC/simple/ParsingUtils.hs
+++ LC/simple/ParsingUtils.hs
@@ -13,3 +13,13 @@ getSymbol =
         return $ first : rest
 
     where underscore = fmap head $ getString "_"
+
+
+withParens parser =
+    do
+        getString "("
+        many getWhiteSpace
+        value <- parser 
+        many getWhiteSpace
+        getString ")"
+        return value
