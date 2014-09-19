89f19f00e1a70c8bc2ae341febb738b90bd63b74
David L. Inman <davidlevere@gmail.com>
2014-09-18 22:06:09 -0400

    prog

--- LC/layout/Tokenizer.hs
+++ LC/layout/Tokenizer.hs
@@ -2,4 +2,18 @@
 module Tokenizer where
 
 import Control.Applicative
+import Parsing
 import LangAst
+import Data.Char
+
+tokenize :: String -> [Token]
+tokenize = undefined
+
+getSymbol =
+    do
+        f <- underscore <|> getAnyAlpha
+        r <- many $ underscore <|> getAnyAlpha <|> (fmap intToDigit getAnyDigit)
+        return $ Symbol (f:r)
+
+
+    where underscore = fmap head (getString "_")
