78f4d0b20ee9e8f9bf749029c2dff75d04367b80
David L. Inman <davidlevere@gmail.com>
2014-11-08 10:27:06 -0500

    incremental

--- syntax/Tokenizer.hs
+++ syntax/Tokenizer.hs
@@ -15,40 +15,29 @@ tokenize str =
 
 allTokens =
     do
-        ts <- many $ getSymbol
-                  <|> getArrow
-                  <|> getLParen
-                  <|> getRParen
-                  <|> getAssign
-                  <|> getLambda
-                  <|> getNewLine
-                  <|> getSpaces 
+        ts <- many $ getSpaces
+                  <|> getNewLine  
+                  <|> getSymbol
         end
         return ts
 
 getSymbol =
     do
         f <- underscore <|> getAnyAlpha
-        r <- many $ underscore <|> getAnyAlpha <|> (fmap intToDigit getAnyDigit)
+        r <- many $ underscore 
+                <|> getAnyAlpha 
+                <|> (fmap intToDigit getAnyDigit)
+                <|> getString "'"
         return $ Symbol (f:r)
 
     where underscore = fmap head (getString "_")
+
 
 getSimple :: String -> Token -> Parser String Token
 getSimple s t =
     do 
         getString s
         return t
-
-getArrow = getSimple "->" Arrow
-
-getLParen = getSimple "(" LParen
-
-getRParen = getSimple ")" RParen
-
-getAssign = getSimple "=" Assign
-
-getLambda = getSimple "\\" Lambda
 
 getNewLine = (getSimple "\r\n" NewLine) <|> (getSimple "\r" NewLine) <|> (getSimple "\n" NewLine)
 
