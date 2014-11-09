edf238dcfba16b516e41dead4b214de275c73a7f
David L. Inman <davidlevere@gmail.com>
2014-11-08 22:36:23 -0500

    cleaned out lang ast

--- experiments/simple/LangAst.hs
+++ experiments/simple/LangAst.hs
@@ -2,33 +2,3 @@
 module LangAst where
 
 
--- instead of lambda term is probably a better choice
-data Lambda =
-    Var String
-    | Abs String Lambda
-    | App Lambda Lambda
-
-    deriving (Show, Eq)
-
-
--- probably a better name for this than assignment (term def?)
-data Assignment = Assignment String Lambda 
-
-    deriving (Show, Eq)
-
-
-data Type = 
-    TypeName String
-    | TypeArrow Type Type 
-    
-    deriving (Show, Eq)
-
-data ConsDef = ConsDef { consName :: String
-                       , parameters :: [Type]
-                       }
-    deriving (Show, Eq)
-
-data DataDef = DataDef { typeName :: String
-                       , cons :: [ConsDef]
-                       }
-    deriving (Show, Eq)
