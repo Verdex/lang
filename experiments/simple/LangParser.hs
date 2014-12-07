06d5bf6eb24b691dc7c04399b9dab672899a85b9
David L. Inman <davidlevere@gmail.com>
2014-12-06 22:50:59 -0500

    guidline

--- experiments/simple/LangParser.hs
+++ experiments/simple/LangParser.hs
@@ -18,11 +18,26 @@ pTypeDef =
         pSimple Data
         typeName <- pAnySymbol
         pSimple Assign
+        
         -- get many cons + param until ; (and | in between)
         return $ TypeDef { td_name = typeName
                          , td_cons = []
                          }   
+ 
+ -- data x = x ;
+ -- data x = y ;
+ -- data x = y | z ;
+ -- data x = y | z | w ;
+ -- data x = y a ;
+ -- data x = y a b ;
+ -- data x = y a b c;
+ -- data x = y x;
+ -- data x = y a b | z a b c;
+ -- data x = y a b | z a | w ;
+ -- data x = y (a -> b) ;
+ -- data x = y (a -> b) c | z d ;
 
+ -- TODO will need type sig parser so I can get arbitrary types of constructor parameters parsed
 
 pAnySymbol :: Parser [Token] String
 pAnySymbol = getAnyX matchSymbol projSymbol
