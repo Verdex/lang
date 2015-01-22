095d8a798f6f738294891cee1f4700d26fa381cf
David L. Inman <davidlevere@gmail.com>
2015-01-21 22:25:07 -0500

    added AST for language

--- experiments/minF/LangAst.hs
+++ experiments/minF/LangAst.hs
@@ -12,3 +12,31 @@ data Token = Symbol String
            | Type 
     deriving (Show, Eq)
 
+data Program = Program [TopLevel]
+             | ParseError
+    deriving (Show, Eq)
+
+data TopLevel = Define ValueDef 
+              | DefineSig ValueSig
+              | TypeDefine TypeDef 
+    deriving (Show, Eq)
+
+data Expr = EVar String
+          | EAbs [String] Expr
+          | EApp Expr Expr
+          | Undefined
+    deriving (Show, Eq)
+
+data ValueSig = ValueSig String TypeSig
+    deriving (Show, Eq)
+
+data ValueDef = ValueDef String Expr
+    deriving (Show, Eq)
+
+data TypeDef = TypeDef String [String]
+    deriving (Show, Eq)
+
+data TypeSig = TSingle String
+             | TypeApp String [TypeSig]
+             | TArrow TypeSig TypeSig 
+    deriving (Show, Eq)
