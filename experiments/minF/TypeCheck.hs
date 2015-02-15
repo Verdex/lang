e57cc74983626a6a466776bd0fa25a8875481719
David L. Inman <davidlevere@gmail.com>
2015-02-14 23:00:11 -0500

    work in progress

--- experiments/minF/TypeCheck.hs
+++ experiments/minF/TypeCheck.hs
@@ -2,6 +2,7 @@
 
 module TypeCheck where
 
+import Control.Applicative
 import LangAst
 -- x : int
 -- blah : int
@@ -39,7 +40,44 @@ import LangAst
 --                    ( \ a -> a ) ( \ a -> a ) => app (forall a . a -> a) (forall a . a -> a)
 --                      although it looks like a sufficiently advanced lookup might be able to handle that
 
+type Lookup = String -> Maybe Type
+
+-- if you have an app that generates a function then our type sig will be describing
+-- params that dont exist in the current lexical scope.  we stil need to type check
+-- but we dont need to worry about looking up anything that those sigs describe (not in lex scope).
 
 
 
+-- blah : x -> y -> z
+-- blah = someFunc (\ w -> ... )
+-- for that case none of the blah type anotation can possibly apply 
+-- to the lambda
 
+-- blah : x -> y -> z
+-- blah = \ w -> \ ww -> ...
+-- here we know that the type annotation applies
+
+-- t1 = forall a . forall b . a -> b -> a
+-- t2 = X
+-- t3 = Y
+-- (\ x -> \ y -> x) X Y
+
+
+typeOf :: Lookup -> Expr -> Maybe Type
+typeOf env (EVar name) = env name
+typeOf env (EApp e1 e2) = 
+    let t1 = typeOf env e1 in
+    let t2 = typeOf env e2 in
+         mu $ morph <$> t1 <*> t2 
+
+    where morph (Arrow input output) t
+              | input == t = Just output 
+              | otherwise = Nothing
+          morph (Forall i inner) t = morph (sub i inner) t
+          morph _ _ = Nothing 
+
+          mu Nothing = Nothing
+          mu (Just Nothing) = Nothing
+          mu (Just (Just t)) = Just t
+
+          sub = undefined
