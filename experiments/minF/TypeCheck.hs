1fc5bf3fc86de417e260e95280ef0bfb4e33241f
David L. Inman <davidlevere@gmail.com>
2015-02-17 23:09:10 -0500

    finished but untested app case

--- experiments/minF/TypeCheck.hs
+++ experiments/minF/TypeCheck.hs
@@ -73,11 +73,17 @@ typeOf env (EApp e1 e2) =
     where morph (Arrow input output) t
               | input == t = Just output 
               | otherwise = Nothing
-          morph (Forall i inner) t = morph (sub i inner) t
+          morph (Forall i inner) t = morph (sub t i inner) t
           morph _ _ = Nothing 
 
           mu Nothing = Nothing
           mu (Just Nothing) = Nothing
           mu (Just (Just t)) = Just t
 
-          sub = undefined
+          sub replace i (Variable n)
+              | i == n = replace
+              | otherwise = Variable n
+          sub replace i (Indexed n ts) = Indexed n $ map (sub replace i) ts
+          sub replace i (Arrow t1 t2) = Arrow (sub replace i t1) (sub replace i t2)
+          sub replace i (Forall n inner) = Forall n (sub replace i inner)
+          sub _ _ (Simple n) = Simple n
