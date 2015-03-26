961137a031190212115d9ad3362fd7a0e074045f
David L. Inman <davidlevere@gmail.com>
2015-03-25 23:56:12 -0400

    typeof implementation for var and abs

--- experiments/TypeSimple/Simple.hs
+++ experiments/TypeSimple/Simple.hs
@@ -2,17 +2,33 @@
 module Simple where
 
 import Control.Applicative
+import State
 
 data Expr = EVar String
           | EAbs String Expr
           | EApp Expr Expr
     deriving (Show, Eq)
 
-data Type = Simple String
-          | Arrow Type Type
+data Type = TSimple String
+          | TArrow Type Type
+          | TVar Integer
     deriving Show
 
+type Env = [(String, Type)]
 
--- add environment? (  [(String, Type)] ... lookup is str -> type )
-typeOf :: Expr -> Maybe Type
-typeOf = undefined
+typeOf :: Env -> Expr -> Maybe Type
+typeOf env expr = evalState (typeOf' env expr) 0
+    where 
+        typeOf' env (EVar varName) = pure $ lookup varName env
+        typeOf' env (EAbs paramName expr) =
+            do
+                i <- newInteger
+                exprType <- typeOf' ( (paramName, TVar i) : env ) expr
+                return $ TArrow (TVar i) <$> exprType 
+
+        typeOf' env (EApp e1 e2) = undefined
+
+
+newInteger :: State Integer Integer
+newInteger = State $ \ i -> (i + 1, i)
+
