a5de2dc15040e994099f4246e26264687861f18d
David L. Inman <davidlevere@gmail.com>
2015-04-02 22:14:13 -0400

    removed test for impossible case
    fixed some formatting

--- experiments/TypeSimple/UnifyTest.hs
+++ experiments/TypeSimple/UnifyTest.hs
@@ -26,47 +26,51 @@ test (result, test, name)
 main = 
     do
         test (unify [] (Constant "a") 
-                        (Constant "b"), fails, "constant mismatch")
+                       (Constant "b"), fails, "constant mismatch")
     
         test (unify [] (Constant "a") 
-                        (Constant "a"), works, "constant match")
+                       (Constant "a"), works, "constant match")
 
         test (unify [] (Function "a" []) 
-                        (Function "b" []), fails, "function name mismatch")
+                       (Function "b" []), fails, "function name mismatch")
 
         test (unify [] (Function "a" []) 
-                        (Function "a" []), works, "function name match, no args")
+                       (Function "a" []), works, "function name match, no args")
 
         test (unify [] (Function "a" [Constant "c"]) 
-                        (Function "a" []), fails, "function name match, arity mismatch")
+                       (Function "a" []), fails, "function name match, arity mismatch")
 
         test (unify [] (Function "a" [Constant "c"])
-                        (Function "a" [Constant "c"]), works, "function name match, args match")
+                       (Function "a" [Constant "c"]), works, "function name match, args match")
 
         test (unify [] (Function "a" [Constant "c", Constant "d"]) 
-                        (Function "a" [Constant "c", Constant "d"]), works, "function name match, args match")
+                       (Function "a" [Constant "c", Constant "d"]), works, "function name match, args match")
 
         test (unify [] (Function "a" [Constant "c", Constant "d"]) 
-                        (Function "a" [Constant "c", Constant "e"]), fails, "function args mismatch")
+                       (Function "a" [Constant "c", Constant "e"]), fails, "function args mismatch")
 
         test (unify [] (Function "a" [Function "b" [Constant "c"]])
-                        (Function "a" [Function "b" [Constant "c"]]), works, "nested functions match")
+                       (Function "a" [Function "b" [Constant "c"]]), works, "nested functions match")
 
         test (unify [] (Function "a" [Function "b" [Constant "c"]])
-                        (Function "a" [Function "b" [Constant "d"]]), fails, "nested functions mismatch args")
+                       (Function "a" [Function "b" [Constant "d"]]), fails, "nested functions mismatch args")
               
         test (unify [] (Function "a" [Function "b" [Constant "c"]])
-                        (Function "a" [Function "c" [Constant "c"]]), fails, "nested functions mismatch")
+                       (Function "a" [Function "c" [Constant "c"]]), fails, "nested functions mismatch")
 
         test (unify [] (Variable 1) 
-                        (Variable 2),     contains (1, Variable 2)
-                                      <+> contains (2, Variable 1), "uninitialized variable unify")
+                       (Variable 2),     contains (1, Variable 2)
+                                     <+> contains (2, Variable 1), "uninitialized variable unify")
 
         -- Self unification seems a bit odd anyway, but you self unify twice then you'll get 
         -- an infinite loop if you actually record the association in ENV
         test (unify [] (Variable 1)
-                        (Variable 1), freeOf (1, Variable 1), "self unification doesnt happen")
+                       (Variable 1), freeOf (1, Variable 1), "self unification doesnt happen")
 
+        -- Warning:  This test case might fail by infinite looping
+        test (unify [(1, Variable 2),
+                     (2, Variable 1)] (Variable 1) 
+                                      (Variable 2), works, "double alias avoids infinite loop")
 
 m = unify [(1, Constant "blah")] (Variable 1) 
                                   (Variable 2)
@@ -82,13 +86,8 @@ p = unify [(1, Variable 3),
             (2, Variable 4)] (Variable 1)
                              (Variable 2)
 
-q = unify [(1, Variable 2),
-            (2, Variable 1)] (Variable 1) -- this causes infinite loop
-                             (Variable 2)
 
 
-s = unify [(1, Variable 1)] (Variable 1) -- this causes infinite loop
-                             (Variable 1)
 
 t = unify [(3, Constant "a"), 
             (1, Variable 3)] (Variable 1)
