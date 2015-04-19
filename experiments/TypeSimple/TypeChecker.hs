6ea512109f5d5a75b8dc010aec18c506b4843191
David L. Inman <davidlevere@gmail.com>
2015-04-18 22:31:08 -0400

    initial stuff dumped into typechecker

--- experiments/TypeSimple/TypeChecker.hs
+++ experiments/TypeSimple/TypeChecker.hs
@@ -1,1 +1,34 @@
 
+module TypeChecker where
+
+import Control.Applicative
+import MState
+
+type TypeEnv = [(String, Type)]
+
+data Expr = EVar String
+          | EAbs String Expr
+          | EApp Expr Expr
+    deriving (Show, Eq)
+
+data Type = TSimple String
+          | TArrow Type Type
+          | TVar Integer
+    deriving (Show, Eq)
+
+
+type VarEnv = [(Integer, Term)]
+
+addToEnv :: Integer -> Term -> MState Env ()
+addToEnv i t = 
+    do
+        env <- getState
+        setState $ (i, t) : env
+
+checkEnv :: Integer -> MState Env (Maybe Term)
+checkEnv i = 
+    do
+        env <- getState
+        return $ lookup i env
+
+
